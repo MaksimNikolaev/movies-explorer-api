58689a2aa8a513cea7811ea8a475d8ed1e5a0b4d
Maksim Nikolaev <mksniklaev@gmail.com>
2022-09-08 12:40:07 +0500

    feat: Implemented logging and error handling

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const { errors } = require('celebrate');
+const cors = require('cors');
+const error = require('./middlewares/error');
 const routes = require('./routes/index');
 const NotFoundError = require('./errors/Not-found-err');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -11,10 +15,20 @@ mongoose.connect('mongodb://localhost:27017/moviesdb');
 
 app.use(express.json());
 
+app.use(requestLogger);
+
+app.use(cors());
+
 app.use(routes); // подключаем роуты
 
 app.use((req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
 });
+
+app.use(errorLogger); // подключаем логгер ошибок
+
+app.use(errors()); // обработчик ошибок celebrate
+
+app.use(error);
 
 app.listen(PORT);
---
+++ middlewares/error.js
@@ -0,0 +1,9 @@
+const error = (err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+
+  const message = statusCode === 500 ? 'На сервере произошла ошибка' : err.message;
+  res.status(statusCode).send({ message });
+  next();
+};
+
+module.exports = error;
